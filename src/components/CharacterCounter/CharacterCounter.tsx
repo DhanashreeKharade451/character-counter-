8cfc9903dc3f12d0e91519cda62ea8efc4e6feda
Dhanashree <dhanashreeakharade@gmail.com>
2026-02-16 20:44:48 -0500

    Working on Charactercount

--- src/components/CharacterCounter/CharacterCounter.tsx
+++ src/components/CharacterCounter/CharacterCounter.tsx
@@ -1,20 +1,8 @@
-//Create a CharacterCounter component that combines the above components and manages the state.
+// //Create a CharacterCounter component that combines the above components and manages the state.
 
-import React, {useState,useEffect} from "react";
-import type { CharacterCounterProps, TextStats } from "../../types";
-import { TextInput } from "../../TextInput";
-import { StatsDisplay } from "../StatsDisplay/StatsDisplay";
+// import React, {useState,useEffect} from "react";
+// import type { CharacterCounterProps, TextStats } from "../../types";
+// import { TextInput } from "../../TextInput";
+// import { StatsDisplay } from "../StatsDisplay/StatsDisplay";
 
 
-export const CharacterCounter: React.FC<CharacterCounterProps> = ({
-    minWord =0,
-    maxWords,
-    targetReadingTime,
-
-})=> 
-    const [text,setText] = useState<string>("");
-    const [stats, setStats] = useState<TextStats>({
-        characterCount: 0,
-        wordCount: 0,
-       ReadingTime: 0,
-    })
--- src/components/StatsDisplay/StatsDisplay.tsx
+++ src/components/StatsDisplay/StatsDisplay.tsx
@@ -1,30 +1,31 @@
 //Create a StatsDisplay component that shows various statistics about the text.
 import React from "react";
 import type { StatsDisplayProps } from "../../types";
+
 export const StatsDisplay: React.FC<StatsDisplayProps> = ({
     stats,
     showReadingTime = true,
 }) => {
-   const {characterCount, wordCount, readingTime} = stats
+   //const {characterCount, wordCount, readingTime} = stats
 
   return (
     <div>
         <div>
             <h3>Characters</h3>
-            <p>Character Count</p>
+            <p>{stats.characterCount}</p>
         </div>
 
         <div>
             <h3>Words</h3>
-            <p>Word Count</p>
+            <p>{stats.wordCount}</p>
         </div>
 
         {showReadingTime && (
             <div>
                 <h3>Reading Time</h3>
                 <p>
-                    {Math.floor(readingTime)};
-                    {Math.round((readingTime%1)*60)
+                    {Math.floor(stats.readingTime)};
+                    {Math.round((stats.readingTime%1)*60)
                     .toString()
                     .padStart(2,"0")}
                 </p>
